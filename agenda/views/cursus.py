ea757b8c7b0b7e5e5605e612a2c2c9dfcb0923ab
incubaton <incubatio@gmail.com>
2010-05-06 18:51:51 +0200

    Commentaire

---
+++ agenda/views/cursus.py
@@ -0,0 +1,12 @@
+def add_cursus(request):
+    pass
+
+def delete_cursus(request, cursus_id):
+    pass
+
+def edit_cursus(request, cursus_id):
+    pass
+
+def get_cursus(request, cursus_id):
+    pass
+
