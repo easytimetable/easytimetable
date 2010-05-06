5050a95561cd404ca453cf16cd47274af2183cbb
ametaireau <ametaireau@gmail.com>
2010-05-06 19:04:08 +0200

    Declared places' and plannings' actions

---
+++ agenda/views/places.py
@@ -0,0 +1,17 @@
+"""Places
+"""
+
+def add_place(request):
+    pass
+
+def delete_place(request, place_id):
+    pass
+
+def update_place(request, place_id):
+    pass
+
+def list_places(request):
+    pass
+
+def get_place(request, place_id):
+    pass
---
+++ agenda/views/plannings.py
@@ -0,0 +1,26 @@
+"""List of django views for plannings 
+"""
+
+def get_planning(request, what=None, extra_context={}):
+    """Return the planning for `what`. What determines the method we will use.
+
+    Special parameters can be set in `extra_parameters`.
+
+    """
+
+def add_event(request):
+    pass
+
+def update_event(reques
+    """Add an avent for a class
+    
+    """
+
+def add_course_event(request):
+    pass
+
+def update_course_event(request, class_event_id):
+    pass
+
+def delete_event(request, event_id):
+    pass
