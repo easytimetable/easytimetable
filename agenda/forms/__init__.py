2398ae21ea8ae58a9318f2d6b8a1364cc49534ae
ametaireau <ametaireau@gmail.com>
2010-05-08 16:12:33 +0200

    Add default theme using blueprint css framework

---
+++ agenda/forms/__init__.py
@@ -0,0 +1,1 @@
+from locations import *
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -6,3 +7,10 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^', include('agenda.urls', namespace='agenda')),
 )
+
+# To serve static files. Do not use in production
+if settings.SERVE_STATIC_FILES:
+    urlpatterns += patterns('', 
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', 
+            {'document_root': settings.MEDIA_ROOT}),
+    )
