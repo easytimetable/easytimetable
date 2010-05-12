a4204f52222f01e2a357a89a7e7557a0e0e70c32
incubaton <incubatio@gmail.com>
2010-05-12 17:05:56 +0200

    url.py updated for places

--- agenda/urls.py
+++ agenda/urls.py
@@ -5,11 +5,18 @@ urlpatterns = patterns('agenda.views.locations',
     (r'^universities/$', 'list_universities', {}, 'list_universities'),
     (r'^universities/(?P<university_id>\d+)/delete/$', 
         'delete_university', {}, 'delete_university'),
+    (r'^universities/(?P<university_id>\d+)$', 'get_university', {}, 'get_university'),
     (r'^campuses/add/$', 'add_campus', {}, 'add_campus'),
     (r'^campuses/$', 'list_campuses', {}, 'list_campuses'),
     (r'^campuses/(?P<campus_id>\d+)$', 'get_campus', {}, 'get_campus'),
     (r'^campuses/(?P<campus_id>\d+)/delete/$', 
         'delete_campus', {}, 'delete_campus'),
+    (r'^places/add/$', 'add_place', {}, 'add_place'),
+    (r'^places/$', 'list_places', {}, 'list_places'),
+    (r'^places/(?P<place_id>\d+)$', 'get_place', {}, 'get_place'),
+    (r'^places/(?P<place_id>\d+)/delete/$',
+         'delete_place', {}, 'delete_place'),
+
 )
 
 urlpatterns += patterns('agenda.views.pedagogy',
