ad285fbbb58bf022f33b11abbeb42dccce39a12b
Guillaume ESPANEL <guillaume.espanel@gmail.com>
2010-05-26 20:57:20 +0200

    Modified the managers

--- agenda/managers.py
+++ agenda/managers.py
@@ -2,7 +2,30 @@ import datetime
 
 from django.db import models
 
-class EventManager(models.Manager):
-    def for_user(self, user):
-        return self.get_query_set().filter
+class WhenManager(models.Manager):
+    
 
+
+    
+    
+    def user_planning(self, user, what):
+        if what == "classgroup":
+            return  self.classgroup_planning(user.get_profile.classgroup.id)
+
+
+
+    #User planning access methods
+    def personal_planning(self, user):
+        pass
+
+    def campus_planning(self, campus_id):
+        pass
+
+    def university_planning(self, university_id):
+        pass
+
+    def studyperiod_planning(self, studyperiod_id):
+        pass
+
+    def classgroup_planning(self, classgroup_id):
+        pass
