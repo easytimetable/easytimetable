0fdbd12c242449193e7ea6ca0574b72afacbe33e
ametaireau <ametaireau@gmail.com>
2010-05-31 23:25:30 +0200

    A beginning of work with calendars and ajaxs forms.

---
+++ agenda/forms/plannings.py
@@ -0,0 +1,14 @@
+from django import forms
+from agenda.models import Event
+
+class UserEventForm(forms.Form):
+    name = forms.CharField(label="Event name")
+    date = forms.DateField(widget=forms.DateInput(attrs={'class':'datepicker'}))
+    duration = forms.IntegerField(label="Event duration", help_text="in hours")
+    place_text = forms.CharField(label="Place", required=False)
+
+class ClassEventForm(forms.Form):
+    def __init__(self, classgroup=None, cursus=None, campus=None, 
+        university=None, *args, **kwargs):
+        if classgroup:
+            pass
--- agenda/views/plannings.py
+++ agenda/views/plannings.py
@@ -1,3 +1,7 @@
+"""List of django views for planning management
+
+"""
+
 # django imports
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import ugettext as _
@@ -6,13 +10,12 @@ from django.http import HttpResponse
 from agenda.models import When
 from django.utils import simplejson as json
 
+# python imports
 import functools
-# app import
+
+# app imports
 from agenda.views import render_to_response
-
-"""List of django views for planning management
-
-"""
+from agenda.forms.plannings import UserEventForm 
 
 def get_planning(request, what=None, extra_context={}, **kwargs):
     """Return the planning for `what`. What determines the method we will use
@@ -31,8 +34,15 @@ def get_planning(request, what=None, extra_context={}, **kwargs):
         d = [p.to_fullcalendar_dict(partial_is_editable) for p in w]
         return HttpResponse(json.dumps(d))
 
-def add_event(request):
-    pass
+def add_user_event(request):
+    if request.POST:
+        form = UserEventForm(data=request.POST)
+    else:
+        form = UserEventForm()
+
+    return render_to_response('agenda/plannings/add_user_event.html', {
+        'form': form,
+    }, request)
 
 def update_event(request):
     pass
