e0dd88f6a1f44a80c4cd0bcc407269618f8df6ef
Guillaume ESPANEL <guillaume.espanel@gmail.com>
2010-06-01 23:18:43 +0200

    Improved the SelectableTimeWidget

--- agenda/forms/plannings.py
+++ agenda/forms/plannings.py
@@ -6,7 +6,8 @@ class UserEventForm(forms.Form):
     name = forms.CharField(label="Event name")
     date = forms.DateField(widget=forms.DateInput(attrs={'class':'datepicker'}))
     start_hour = forms.TimeField(widget=SelectableTimeWidget())
-    duration = forms.IntegerField(label="Event duration", help_text="in hours")
+    duration = forms.TimeField(widget=SelectableTimeWidget(end_hour=5),
+                    help_text="in hours")
     place_text = forms.CharField(label="Place", required=False)
 
 class ClassEventForm(forms.Form):
--- utils/widgets.py
+++ utils/widgets.py
@@ -5,9 +5,20 @@ class SelectableTimeWidget(widgets.Select):
     """ Displays a list of hours. (24)
 
     """
-    def render(self, name, value, attrs=None, choices=([('', ' -- ')] + 
-        [(hour,str(hour).zfill(2) + 'h') for hour in range(0, 24)])):
+    start_hour = None
+    end_hour = None
+
+    def __init__(self, start_hour=0, end_hour=24, *args, **kwargs):
+        super(SelectableTimeWidget,self).__init__(*args, **kwargs)
+        self.start_hour = start_hour
+        self.end_hour = end_hour
+
+
+    def render(self, name, value, attrs=None, choices=None):
         """Returns this Widget rendered as HTML, as a Unicode string."""
+        if choices == None:
+            choices = [(hour,str(hour).zfill(2) + 'h') for hour in\
+              range(self.start_hour,self.end_hour)] 
         if isinstance(value, datetime.time):
             value = value.hour    
         return super(SelectableTimeWidget, self).render(name, value, attrs,
