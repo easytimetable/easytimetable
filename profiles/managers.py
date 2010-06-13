0b3771dafc4e453eb2fc621937ce533bbecdc48d
Guillaume ESPANEL <guillaume.espanel@gmail.com>
2010-06-13 02:03:04 +0200

    breakpoint removed

--- profiles/managers.py
+++ profiles/managers.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class ClassGroupManager(models.Manager):
     def get_managed_by(self, user=None):
-        from ipdb import set_trace; set_trace()
         if user.is_staff:
             return super(ClassGroupManager, self).get_query_set().all()
         return super(ClassGroupManager,
